36d9d778a105e36e22e89dab977751d9aaefe212
Anastasia <80560435+Stasies@users.noreply.github.com>
2021-09-13 10:17:00 +0500

    Add files via upload

---
+++ precache-manifest.c52db117bd6f34465bed6b93d32a640a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a682b3b92a0a6bd55ed644946488c9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdac087d205127541b23",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "eca6562c3e8f4e50dcb6",
+    "url": "/static/js/2.51d1ca9a.chunk.js"
+  },
+  {
+    "revision": "fdac087d205127541b23",
+    "url": "/static/js/main.d6bf51a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
